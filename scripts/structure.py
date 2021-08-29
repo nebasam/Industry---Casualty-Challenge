cb6af175b1f467be75fc4435c3baf9f41c15c071
Nebiyu Samuel <neba.samuel17@gmail.com>
2021-08-29 10:51:29 +0300

    modify causal notebook

--- scripts/structure.py
+++ scripts/structure.py
@@ -1,11 +1,37 @@
 from causalnex.plots import plot_structure, NODE_STYLE, EDGE_STYLE
 from logger import Logger
+import mlflow
+from IPython.display import Image
+import os
 class Structure:
     
-    def __init__(self):
-        self.logger = Logger().get_logger(__name__)
+    def drawgraph(self,sm,path):
+        """Draws Causal graph
+    Args:
+        structural_model (from_pandas_lasso): Structural model of causalnex
+    Returns:
+        plot_structure
+    """
     
-    def drawgraph (self,sm):
+        try:
+            mlflow.set_experiment('Breast cancer Causality')
+            sm.remove_edges_below_threshold(0.8)
+            viz = plot_structure( sm, graph_attributes={"scale": "2.0","size": "2.5"}, all_node_attributes=NODE_STYLE.WEAK, all_edge_attributes=EDGE_STYLE.WEAK)
+            img = Image(viz.draw(format='png'))
+            self.logger.info(f'plots causal graph successfully')
+            if (not os.path.isdir('../output')):
+                os.mkdir("../output")
+            print("writing graph image")
+            with open(f"path", "wb") as png:
+                png.write(img.data)
+            mlflow.log_artifact(path)
+
+            return img
+        
+        except Exception:
+            self.logger.exception('plots causal graph  failed.')
+        
+    def drawgraphs(self,sm,path):
         """Draws Causal graph
     Args:
         structural_model (from_pandas_lasso): Structural model of causalnex
@@ -13,19 +39,17 @@ class Structure:
         plot_structure
     """
         try:
-            sm.remove_edges_below_threshold(0.8)
+            mlflow.set_experiment('Breast cancer Causality')
             viz = plot_structure( sm, graph_attributes={"scale": "2.0","size": "2.5"}, all_node_attributes=NODE_STYLE.WEAK, all_edge_attributes=EDGE_STYLE.WEAK)
+            img = Image(viz.draw(format='png'))
+            if (not os.path.isdir('../output')):
+                os.mkdir("../output")
+            print("writing graph image")
+            with open(f"path", "wb") as png:
+                png.write(img.data)
+            mlflow.log_artifact(path)
             self.logger.info(f'plots causal graph successfully')
-            return viz
-        
-        except Exception:
-            self.logger.exception('plots causal graph  failed.')
-        
-    def drawgraphs(self,sm):
-        try:
-            viz = plot_structure( sm, graph_attributes={"scale": "2.0","size": "2.5"}, all_node_attributes=NODE_STYLE.WEAK, all_edge_attributes=EDGE_STYLE.WEAK)
-            self.logger.info(f'plots causal graph successfully')
-            return viz
+            return img
         
         except Exception:
             self.logger.exception('plots causal graph  failed.')
